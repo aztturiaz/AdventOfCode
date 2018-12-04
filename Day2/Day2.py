44fcad2375dd8a13d7a3b8ebaacf51e5bec81d1d
jgracia <jorge.gracia@safetyfire.com>
2018-12-04 11:57:43 -0600

    update day2 w/ python
    update

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,1 +1,30 @@
 
+#s = "Hello Visual Studio"
+#print(s)
+#if 'techbeamers'.isidentifier():
+#    print("techbeamers is identifier")
+
+
+
+filepath = 'Data.txt'
+with open(filepath) as fp:
+	line = fp.readline()
+	cnt = 1
+	for line in fp:
+		#print("Line {}: {}".format(cnt, line.strip()))
+		substring = ""
+		charDict = {}
+		for char in line.strip():
+			charDict[char] += 1
+			substring += "{}-".format(char)
+		print("Line {}: {}".format(cnt, substring))
+		line = fp.readline()
+		cnt += 1
+		if cnt == 10:
+			break
+
+
+print("\n\n\n")
+
+
+
