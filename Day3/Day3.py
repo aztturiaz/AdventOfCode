9e0ca08798285c8df07ab8d17cab13bcb0ed1e26
jgracia <jorge.gracia@safetyfire.com>
2018-12-05 17:26:15 -0600

    Update Day3
    Almost done Day3 Part 1

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -2,11 +2,51 @@
 from Rectangle import Rectangle
 
 filepath = 'Data.txt'
-#filepath = 'DataTest.txt'
+filepath = 'DataTest.txt'
 #filepath = 'DataTest2.txt'
 
-r = Rectangle(1, 1, 1, 1)
-r2 = Rectangle(2, 2, 2, 2)
+## ----------------------------------------------------------------------
+r1 = Rectangle(1, 3, 4, 6)
+r2 = Rectangle(3, 1, 6, 4)
 
-print(r)
-r.Intersect(r2)
+r3 = r1.Intersect(r2)
+print('({},{}) to ({},{})'.format(r3.x1, r3.y1, r3.x2, r3.y2))
+print('Intersect Rectangle size = {}'.format(r3.SquareArea()))
+print('Intersect Rectangle size = {}'.format(Rectangle(0, 0, 0, 0).SquareArea()))
+
+## ----------------------------------------------------------------------
+
+claimsDictionary = {}
+
+with open(filepath) as fp:
+	cnt = 1
+
+	for line in fp: #Read each line in the File
+		
+		point = line.split(' ')[2].rstrip(':').split(',')
+		size = line.split(' ')[3].strip().split('x')
+
+		# Create the claim Rectangle
+		rectangle = Rectangle(int(point[0]), int(point[1]), int(point[0]) + (int(size[0]) - 1), int(point[1]) + (int(size[1]) - 1))
+
+		claimsDictionary.update({cnt:rectangle})
+
+		cnt += 1
+
+print('\n\n')
+print(claimsDictionary)
+
+copyClaims = claimsDictionary.copy()
+
+for claim in claimsDictionary.items():
+
+	rec01 = claim[1]
+
+	del copyClaims[claim[0]]
+	for copyClaim in copyClaims.items():
+		continue
+		rec02 = copyClaim[1]
+		#rec01.Intersect
+
+
+
--- Day3/Rectangle.py
+++ Day3/Rectangle.py
@@ -19,18 +19,45 @@
 		newX2 = 0
 		newY2 = 0
 
-		#if self.x1 < r.x1 or r.x2 < self.x2:
-
+		# New X1
 		if r.x1 <= self.x1:
-			if (r.x2 > self.x1 and (r.x2 < self.x2 or r.x2 >= self.x2)): newX1 = self.x1
+			if (r.x2 >= self.x1): newX1 = self.x1
 		else:
-			if (r.x1 < self.x2 and (r.x2 < self.x2 or r.x2 >= self.x2)): newX1 = r.x1
+			if (r.x1 <= self.x2): newX1 = r.x1
 
+		# New X2
 		if r.x2 <= self.x2:
-			if (r.x2 > self.x1 and (r.x1 <= self.x1 or r.x1 > self.x1)): newX2 = self.x1
+			if (r.x2 >= self.x1): newX2 = r.x2
 		else:
-			if (r.x1 < self.x2 and (r.x2 < self.x2 or r.x2 > self.x2)): newX1 = r.x1
+			if (r.x1 <= self.x2): newX2 = self.x2
 
-		print('({},{}) to ({},{})'.format(newX1, newY1, newX2, newY2))
+		# New Y1
+		if r.y1 <= self.y1:
+			if (r.y2 >= self.y1): newY1 = self.y1
+		else:
+			if (r.y1 <= self.y2): newY1 = r.y1
+
+		# New Y2
+		if r.y2 <= self.y2:
+			if (r.y2 >= self.y1): newY2 = r.y2
+		else:
+			if (r.y1 <= self.y2): newY2 = self.y2
+
+		return Rectangle(newX1, newY1, newX2, newY2)
+
+	def SquareArea(self):
+		x2 = (self.x2 + 1) if self.x2 != self.x1 else self.x2
+		x1 = self.x1
+
+		y2 = (self.y2 + 1) if self.y2 != self.y1 else self.y2
+		y1 = self.y1
+
+		width = abs(x2 - x1)
+		height = abs(y2 - y1)
+
+		print('W: {} - H: {}'.format(width, height))
+
+		return width * height
+
 		
 			
