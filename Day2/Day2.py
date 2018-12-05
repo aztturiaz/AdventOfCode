4590b61820349b5e9b8c0eb837e0f63105de0e91
Jorge Gracia <35716948+aztturiaz@users.noreply.github.com>
2018-12-04 22:49:26 -0600

    Day2 Part 1 finished
    Day2 1st part completed

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,30 +1,38 @@
 
-#s = "Hello Visual Studio"
-#print(s)
-#if 'techbeamers'.isidentifier():
-#    print("techbeamers is identifier")
-
-
-
 filepath = 'Data.txt'
+#filepath = 'DataTest.txt'
+
 with open(filepath) as fp:
-	line = fp.readline()
 	cnt = 1
+	checksumDict = {2: 0, 3: 0}
+
 	for line in fp:
-		#print("Line {}: {}".format(cnt, line.strip()))
 		substring = ""
 		charDict = {}
+		foundDoubles = False; foundTriples = False
+
+		
 		for char in line.strip():
-			charDict[char] += 1
-			substring += "{}-".format(char)
-		print("Line {}: {}".format(cnt, substring))
-		line = fp.readline()
+			if charDict.get(char, 0) == 0:
+				charDict.update({char: 1})
+			else:
+				charDict[char] += 1
+		
+		for char in charDict.items():
+			if (char[1] == 2 and not foundDoubles):
+				checksumDict[2] += 1
+				foundDoubles = True
+			if (char[1] == 3 and not foundTriples):
+				checksumDict[3] += 1
+				foundTriples = True
+		
+		#print('Line {}: charDict = {}'.format(cnt, charDict))
 		cnt += 1
-		if cnt == 10:
-			break
+	
+	print('Checksum Dictionary = {}'.format(checksumDict))
+	print('Final CheckSum = {}'.format(checksumDict[2] * checksumDict[3]))
 
-
-print("\n\n\n")
+print('\n\n')
 
 
 
