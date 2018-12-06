ac57b8ea2bea176fa9e100ec5cb5a102798b7fc5
jgracia <jorge.gracia@safetyfire.com>
2018-12-06 16:57:30 -0600

    Setup Day4 and Day6
    Setup

---
+++ Day6/Day6Data.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Day6
+{
+    static public class Day6Data
+    {
+		static public Dictionary<int, Point> data = new Dictionary<int, Point>()
+		{
+			{ 1, new Point(59, 110) },
+			{ 2, new Point(127, 249) },
+			{ 3, new Point(42, 290) },
+			{ 4, new Point(90, 326) },
+			{ 5, new Point(108, 60) },
+			{ 6, new Point(98, 168) },
+			{ 7, new Point(358, 207) },
+			{ 8, new Point(114, 146) },
+			{ 9, new Point(242, 170) },
+			{ 10, new Point(281, 43) },
+			{ 11, new Point(233, 295) },
+			{ 12, new Point(213, 113) },
+			{ 13, new Point(260, 334) },
+			{ 14, new Point(287, 260) },
+			{ 15, new Point(283, 227) },
+			{ 16, new Point(328, 235) },
+			{ 17, new Point(96, 259) },
+			{ 18, new Point(232, 177) },
+			{ 19, new Point(198, 216) },
+			{ 20, new Point(52, 115) },
+			{ 21, new Point(95, 258) },
+			{ 22, new Point(173, 191) },
+			{ 23, new Point(156, 167) },
+			{ 24, new Point(179, 135) },
+			{ 25, new Point(235, 235) },
+			{ 26, new Point(164, 199) },
+			{ 27, new Point(248, 180) },
+			{ 28, new Point(165, 273) },
+			{ 29, new Point(160, 297) },
+			{ 30, new Point(102, 96) },
+			{ 31, new Point(346, 249) },
+			{ 32, new Point(176, 263) },
+			{ 33, new Point(140, 101) },
+			{ 34, new Point(324, 254) },
+			{ 35, new Point(72, 211) },
+			{ 36, new Point(126, 337) },
+			{ 37, new Point(356, 272) },
+			{ 38, new Point(342, 65) },
+			{ 39, new Point(171, 295) },
+			{ 40, new Point(93, 192) },
+			{ 41, new Point(47, 200) },
+			{ 42, new Point(329, 239) },
+			{ 43, new Point(60, 282) },
+			{ 44, new Point(246, 185) },
+			{ 45, new Point(225, 324) },
+			{ 46, new Point(114, 329) },
+			{ 47, new Point(134, 167) },
+			{ 48, new Point(212, 104) },
+			{ 49, new Point(338, 332) },
+			{ 50, new Point(293, 94) }
+		};
+
+		static public Dictionary<int, Point> testData = new Dictionary<int, Point>()
+		{
+			{ 1, new Point(1, 1) },
+			{ 2, new Point(1, 6) },
+			{ 3, new Point(8, 3) },
+			{ 4, new Point(3, 4) },
+			{ 5, new Point(5, 5) },
+			{ 6, new Point(8, 9) }
+		};
+	}
+}
