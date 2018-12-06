9f0e926fdad0873e6877e376863ae69febf7ee1c
Jorge Gracia <35716948+aztturiaz@users.noreply.github.com>
2018-12-06 00:49:08 -0600

    Update Day3 part 2
    Almost finished... get the ID while getting the overlaping fabric. Line 20 - 21

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,32 +1,44 @@
 
 filepath = 'Data.txt'
-#filepath = 'DataTest.txt'
+filepath = 'DataTest.txt'
 
 claimsDictionary = {}
-fabricOverlap = {}
 totalSquareInches = 0
+claimIDList = []
+cnt = 0
 
 with open(filepath) as fp:
-	cnt = 1
+	
 	for line in fp: #Read each line in the File
-		
+		cnt += 1
+
 		point = line.split(' ')[2].rstrip(':').split(',')
 		size = line.split(' ')[3].strip().split('x')
 
 		for x in range(int(point[0]), int(point[0]) + int(size[0]), 1):
 			for y in range(int(point[1]), int(point[1]) + int(size[1]), 1):
-				if fabricOverlap.get('{},{}'.format(x, y)) == None:
-				  fabricOverlap.update({'{},{}'.format(x, y):1})
+				if claimsDictionary.get('{},{}'.format(x, y)) == None:
+				  claimsDictionary.update({'{},{}'.format(x, y):[cnt, 1]})
 				else:
-				  fabricOverlap['{},{}'.format(x, y)] += 1
+				  claimsDictionary['{},{}'.format(x, y)][1] += 1
 
-		cnt += 1
+				#if cnt not in claimIDList:
+				#	claimIDList.append(cnt)
 
-for item in fabricOverlap.items():
-	if item[1] > 1:
-	   totalSquareInches += 1
+# Get the Square Inches of overlaping fabric
+for item in claimsDictionary.items():
+	if item[1][1] > 1:
+		totalSquareInches += 1
+		if item[1][0] not in claimIDList:
+			claimIDList.append(item[1][0])
+
+# Get the Claim ID not overlaping
+claimID = 0	
+for id in range(1, cnt + 1, 1):
+	if id not in claimIDList:
+		claimID = id
 
 print('Total Square Inches = {}'.format(totalSquareInches))
-
+print('Claim Id w/no overlap: {}'.format(claimID))
 
 
