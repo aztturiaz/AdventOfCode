3113b15564e2286e21d9816d1cc5dcd6a5b9ad87
jgracia <jorge.gracia@safetyfire.com>
2018-12-07 17:01:57 -0600

    Day6 - small changes
    Out of ideas

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -21,8 +21,17 @@ namespace Day6
 			Point minBoundary = new Point(data.Values.Min(p => p.X), data.Values.Min(p => p.Y));
 			Point maxBoundary = new Point(data.Values.Max(p => p.X), data.Values.Max(p => p.Y));
 
+			minBoundary.Offset(-1, -1);
+			maxBoundary.Offset(1, 1);
+
 			Console.WriteLine(string.Format("Manhattan Distance between {0} and {1} = {2}", minBoundary, maxBoundary, minBoundary.ManhattanDistance(maxBoundary)));
 
+			// ========================================
+
+			Dictionary<Point, int> coordinates = new Dictionary<Point, int>();
+
+			// ========================================
+			
 			if (data.ContainsKey(10))
 			{
 				Console.WriteLine("Contains Key!");
@@ -33,7 +42,9 @@ namespace Day6
 			}
 			
 			//int[,] matrix = new int[(maxBoundary.X + 1) - minBoundary.X, (maxBoundary.Y + 1) - minBoundary.Y];
-			int[,] matrix = new int[(maxBoundary.Y + 1) - minBoundary.Y, (maxBoundary.X + 1) - minBoundary.X];
+			int[,] matrix = new int[(maxBoundary.X + 1), (maxBoundary.Y + 1)];
+			/////////////////////int[,] matrix = new int[(maxBoundary.Y + 1) - minBoundary.Y, (maxBoundary.X + 1) - minBoundary.X];
+
 
 			for (int x = minBoundary.X; x <= maxBoundary.X; x++)
 			{
@@ -43,7 +54,9 @@ namespace Day6
 					if (data.ContainsValue(origin))
 					{
 						//matrix[x - 1, y - 1] = data.FirstOrDefault(d => d.Value.Equals(origin)).Key * 10;
-						matrix[y - 1, x - 1] = data.FirstOrDefault(d => d.Value.Equals(origin)).Key * 10;
+						//matrix[y - 1, x - 1] = data.FirstOrDefault(d => d.Value.Equals(origin)).Key * 10;
+						matrix[x, y] = data.FirstOrDefault(d => d.Value.Equals(origin)).Key * 10;
+						//matrix[y, x] = data.FirstOrDefault(d => d.Value.Equals(origin)).Key * 10;
 						break;
 					}
 					
@@ -52,9 +65,16 @@ namespace Day6
 					{
 						if (closestDistance == 0 || origin.ManhattanDistance(destiny) < closestDistance)
 						{
+							if (!coordinates.ContainsKey(origin))
+							{
+								coordinates.Add(origin, closestDistance);
+							}
+
 							closestDistance = origin.ManhattanDistance(destiny);
 							//matrix[x - 1, y - 1] = matrix[x - 1, y - 1] > 0 || matrix[x - 1, y - 1] == -1 ? -1 : data.FirstOrDefault(d => d.Value.Equals(destiny)).Key;
-							matrix[y - 1, x - 1] = matrix[y - 1, x - 1] > 0 || matrix[y - 1, x - 1] == -1 ? -1 : data.FirstOrDefault(d => d.Value.Equals(destiny)).Key;
+							//matrix[y - 1, x - 1] = matrix[y - 1, x - 1] > 0 || matrix[y - 1, x - 1] == -1 ? -1 : data.FirstOrDefault(d => d.Value.Equals(destiny)).Key;
+							matrix[x, y] = matrix[x, y] > 0 || matrix[x, y] == -1 ? -1 : data.FirstOrDefault(d => d.Value.Equals(destiny)).Key;
+							//matrix[y, x] = matrix[y, x] > 0 || matrix[y, x] == -1 ? -1 : data.FirstOrDefault(d => d.Value.Equals(destiny)).Key;
 						}
 						
 					}
