17b8c0e36e34583c48e92161d7b34f74ca17037d
Jorge Gracia <35716948+aztturiaz@users.noreply.github.com>
2018-12-07 01:52:52 -0600

    Day 6 Update
    Need to keep a record for all the distances on each point

---
+++ Day6/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Day6
+{
+    public static class Extensions
+    {
+		public static int ManhattanDistance(this Point origin, Point destiny)
+		{
+			return Math.Abs(destiny.X - origin.X) + Math.Abs(destiny.Y - origin.Y);
+		}
+	}
+}
