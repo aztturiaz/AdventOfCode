98043f69fb155b3b6d6d749841a7b1bdee2b5d30
jgracia <jorge.gracia@safetyfire.com>
2018-12-26 13:47:43 -0600

    Day4 Update
    Update

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,1 +1,21 @@
+from datetime import datetime
+filepath = 'Data.txt'
+#filepath = 'DataTest.txt'
 
+
+myDict = {}
+
+with open(filepath) as fp:
+	
+	for line in fp: #Read each line in the File
+		
+		dateTime = datetime.strptime(line.lstrip('[').split(']')[0], '%Y-%m-%d %H:%M')
+		myDict.update({dateTime : line.lstrip('[').split(']')[1].strip()})
+
+
+
+for key in sorted(myDict):
+  print('{0} - {1}'.format(key, myDict[key]))
+
+#print(myDict)
+print('\n')
